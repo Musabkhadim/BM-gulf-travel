9b78a593b026d0f898079796fdc7469b16eba9f5
Musab Khadim <musabkhadim363@gmail.com>
2025-10-26 14:20:40 +0500

    don

--- app/firebase/firebase.tsx
+++ app/firebase/firebase.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdojOtAwMekR6TMl3TGtivOkgmxbUVnCo",
+ apiKey: "AIzaSyDdojOtAwMekR6TMl3TGtivOkgmxbUVnCo",
   authDomain: "bloge-2272e.firebaseapp.com",
   projectId: "bloge-2272e",
   storageBucket: "bloge-2272e.firebasestorage.app",
   messagingSenderId: "831432305568",
   appId: "1:831432305568:web:dd7f50442065c939bb52c5",
-  measurementId: "G-03N1CD7XYS"
+
 };
 
 // Initialize Firebase
